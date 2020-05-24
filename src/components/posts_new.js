964f6e353c604a721764109030323f7413bc795a
shreyaskargit <shreyaskar30@gmail.com>
2020-05-24 21:34:44 +0530

    adding Field in redux form

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,23 +1,23 @@
 import React, { Component } from "react";
-import { reduxForm } from "redux-form";
+import { reduxForm, Field } from "redux-form";
 import { createPost } from "../actions/index";
 import { connect } from "react-redux";
 
 class PostsNew extends Component {
   state = {};
   onSubmit = (formValues) => {
-    console.log("hello", formValues);
+    console.log(formValues);
   };
   render() {
     const {
-      fields: { title, categories, content },
+      // fields: { title, categories, content },
       handleSubmit,
     } = this.props;
     // console.log(this.props.createPost);
     return (
       <form onSubmit={handleSubmit(this.onSubmit)}>
         <h3>Create A New Post</h3>
-        <div className="form-group">
+        {/* <div className="form-group">
           <label>Title</label>
           <input name="title" type="text" className="form-control" {...title} />
         </div>
@@ -35,6 +35,21 @@ class PostsNew extends Component {
           <textarea name="content" className="form-control" {...content} />
         </div>
         <button type="submit" className="btn btn-primary">
+          Submit
+        </button> */}
+        <div>
+          <label htmlFor="title">Title</label>
+          <br /><Field name="title" id="title" component="input" type="text"/>
+        </div>
+        <div>
+          <label htmlFor="categories">Categories</label>
+          <br /><Field name="categories" id="categories" component="input" type="text"/>
+        </div>
+        <div>
+          <label htmlFor="description">Description</label>
+          <br /><Field name="description" id="description" component="textarea" />
+        </div>
+        <button type="submit">
           Submit
         </button>
       </form>
