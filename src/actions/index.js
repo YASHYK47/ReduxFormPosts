959701f6f044663ebc6a3dbf77c75878d1cff6d1
YASHYK47 <yashky47@gmail.com>
2020-05-26 03:54:45 +0530

    Delete Post and its routing

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,22 +3,27 @@ import axios from "axios";
 export const FETCH_POST = "FETCH_POST";
 export const FETCH_POSTS = "FETCH_POSTS";
 export const CREATE_POST = "CREATE_POST";
+export const DELETE_POST = "DELETE_POST";
 
 const BASE_URL = "http://reduxblog.herokuapp.com/api";
 const API_KEY = "?key=123";
 
+export function createPost(props) {
+  const response = axios.post(`${BASE_URL}/posts${API_KEY}`, props);
+  return { type: CREATE_POST, payload: response };
+}
+
 export function fetchPosts() {
   const response = axios.get(`${BASE_URL}/posts${API_KEY}`);
-  return { type: "FETCH_POSTS", payload: response };
+  return { type: FETCH_POSTS, payload: response };
 }
 
 export function fetchPost(post_id) {
-  console.log("called");
   const response = axios.get(`${BASE_URL}/posts/${post_id}${API_KEY}`);
-  return { type: "FETCH_POST", payload: response };
+  return { type: FETCH_POST, payload: response };
 }
 
-export function createPost(props) {
-  const response = axios.post(`${BASE_URL}/posts${API_KEY}`, props);
-  return { type: "CREATE_POST", payload: response };
+export function deletePost(post_id) {
+  const response = axios.delete(`${BASE_URL}/posts/${post_id}${API_KEY}`);
+  return { type: DELETE_POST, payload: response };
 }
--- src/components/posts_view.js
+++ src/components/posts_view.js
@@ -1,26 +1,43 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router";
+import PropTypes from "prop-types";
 
-import { fetchPost } from "../actions/index";
+import { fetchPost, deletePost } from "../actions/index";
 
 class PostsView extends Component {
+  static contextTypes = {
+    router: PropTypes.object,
+  };
   componentDidMount() {
     this.props.fetchPost(this.props.params.post_id);
   }
+  handleClick = () => {
+    this.props.deletePost(this.props.post.id).then(() => {
+      this.context.router.push("/");
+    });
+  };
   render() {
     const { post } = this.props;
     if (!post) return <div></div>;
     return (
       <div>
-        <div className="text-xs-right">
-          <Link className="btn btn-primary" to="/">
+        <div style={{ margin: "20px 0" }}>
+          <Link to="/" className="btn btn-primary">
             Back to Index
           </Link>
         </div>
         <h1>{post.title}</h1>
         <h6>{post.categories}</h6>
         <p>{post.content}</p>
+        <div style={{ margin: "20px 0" }}>
+          <button
+            className="btn btn-danger"
+            onClick={this.handleClick.bind(this)}
+          >
+            Delete Post
+          </button>
+        </div>
       </div>
     );
   }
@@ -30,4 +47,4 @@ function mapStateToProps(state) {
   return { post: state.posts.post };
 }
 
-export default connect(mapStateToProps, { fetchPost })(PostsView);
+export default connect(mapStateToProps, { fetchPost, deletePost })(PostsView);
